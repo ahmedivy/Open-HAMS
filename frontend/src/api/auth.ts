0bc1cecf5f20847f057b0eb3893f64f9e350359f
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-07-09 13:12:24 +0500

    add separate axios instance for login and signup

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,5 +1,18 @@
 import { LoginSchema, SignUpSchema } from "@/api/schemas/auth";
-import instance from "./axios";
+import axios from "axios";
+import { API_URL } from "./utils";
+
+const instance = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeoutErrorMessage: "Request timed out",
+  validateStatus(_) {
+    return true;
+  },
+});
 
 export async function register(values: SignUpSchema) {
   const res = await instance.post("/users/", values);
