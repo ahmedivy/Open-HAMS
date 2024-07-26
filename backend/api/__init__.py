2ac5bc1582eeeef9ba219ecab94feea2ffb757b3
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-07-26 17:27:57 +0500

    add reports

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
 
+from .routes.admin import router as admin_router
 from .routes.animals import router as animals_router
 from .routes.event_types import router as event_types_router
 from .routes.events import router as events_router
 from .routes.groups import router as groups_router
 from .routes.roles import router as roles_router
+from .routes.upload import router as upload_router
 from .routes.users import router as users_router
 from .routes.zoo import router as zoo_router
-from .routes.upload import router as upload_router
 
 api_router = APIRouter()
 
@@ -19,3 +20,4 @@ api_router.include_router(event_types_router)
 api_router.include_router(groups_router)
 api_router.include_router(roles_router)
 api_router.include_router(upload_router)
+api_router.include_router(admin_router)
